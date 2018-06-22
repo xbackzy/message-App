6de11409f30bba0a227cafe1ecce9b87918ff0e7
Your Name <you@example.com>
2018-06-22 11:23:29 +0800

    Second

---
+++ src/DataServer/UserMessage.js
@@ -0,0 +1,51 @@
+import{loginURL,registerURL} from './URLConfig';
+class UserMessage{
+    async login(username,password){
+        try {
+            const user={
+                username,
+                password
+            }
+
+            const res=await fetch(loginURL,{
+                method:'POST',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(user)
+            });
+            const result=await res.json();
+            if(result.success==true){
+                localStorage.access_token=result.data.access_token
+            }
+            return result;
+        } catch (error) {
+            return{
+                success:false,
+                errorMessage:'网络错误'
+            }
+        }
+    }
+
+    async register(username,password){
+        try {
+            const user={
+                username,
+                password
+            }
+
+            const res=await fetch(registerURL,{
+                method:'POST',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(user)
+            })
+        } catch (error) {
+            
+        }
+    }
+}
+export default new UserMessage();
